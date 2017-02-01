26a24696ca35891a39bce15c267dc9108d62a87a
Victor Jimenez <vjimenez@americatv.com.pe>
2017-02-01 14:36:10 -0500

    add

---
+++ Laundry/Laundry/BdComun.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApplication1
+{
+    public class BdComun
+    {
+        public static MySqlConnection ObtenerConexion()
+        {
+            MySqlConnection conectar = new MySqlConnection("server=cualesmiip.pe; database=cualesmi_api; Uid=cualesmi_web; pwd=vji2002;");
+            conectar.Open();
+            return conectar;
+        }
+    }
+}
---
+++ Laundry/Laundry/Models/Prenda.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1.Models
+{
+    public class Prenda
+    {
+        public int idPrenda { get; set; }
+        public string NombrePrenda { get; set; }
+        public string Descripcion { get; set; }
+        public float precioServicio { get; set; }
+
+    }
+}
