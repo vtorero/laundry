f7e257602affbc3b8e24168bb86dacb52cc10b5e
Victor Jimenez <vtorero@gmail.com>
2017-02-02 22:43:05 -0500

    casa

--- Laundry/Laundry/forms/frmPrendas.Designer.cs
+++ Laundry/Laundry/forms/frmPrendas.Designer.cs
@@ -34,29 +34,35 @@
             this.txtPrecio = new System.Windows.Forms.TextBox();
             this.dgvPrenda = new System.Windows.Forms.DataGridView();
             this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvPrenda)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(21, 269);
+            this.button1.Location = new System.Drawing.Point(370, 269);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(167, 50);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Grabar";
+            this.button1.Text = "&Grabar";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // txtNombre
             // 
-            this.txtNombre.Location = new System.Drawing.Point(79, 42);
+            this.txtNombre.Location = new System.Drawing.Point(138, 19);
             this.txtNombre.Name = "txtNombre";
             this.txtNombre.Size = new System.Drawing.Size(215, 20);
             this.txtNombre.TabIndex = 1;
             // 
             // txtDescripcion
             // 
-            this.txtDescripcion.Location = new System.Drawing.Point(79, 68);
+            this.txtDescripcion.Location = new System.Drawing.Point(138, 45);
             this.txtDescripcion.Multiline = true;
             this.txtDescripcion.Name = "txtDescripcion";
             this.txtDescripcion.Size = new System.Drawing.Size(215, 53);
@@ -65,9 +71,9 @@
             // 
             // txtPrecio
             // 
-            this.txtPrecio.Location = new System.Drawing.Point(79, 127);
+            this.txtPrecio.Location = new System.Drawing.Point(138, 104);
             this.txtPrecio.Name = "txtPrecio";
-            this.txtPrecio.Size = new System.Drawing.Size(126, 20);
+            this.txtPrecio.Size = new System.Drawing.Size(82, 20);
             this.txtPrecio.TabIndex = 3;
             // 
             // dgvPrenda
@@ -80,7 +86,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(194, 269);
+            this.button2.Location = new System.Drawing.Point(21, 269);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(154, 50);
             this.button2.TabIndex = 5;
@@ -88,23 +94,73 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtPrecio);
+            this.groupBox1.Controls.Add(this.txtDescripcion);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Location = new System.Drawing.Point(31, 20);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(506, 135);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nombre prenda:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Detalle adicional:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Precio x Servicio:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(181, 269);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(183, 50);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "&Eliminar";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // frmPrendas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(559, 335);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.dgvPrenda);
-            this.Controls.Add(this.txtPrecio);
-            this.Controls.Add(this.txtDescripcion);
-            this.Controls.Add(this.txtNombre);
             this.Controls.Add(this.button1);
             this.Name = "frmPrendas";
             this.Text = "frmPrendas";
             this.Load += new System.EventHandler(this.frmPrendas_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvPrenda)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -116,5 +172,10 @@
         private System.Windows.Forms.TextBox txtPrecio;
         private System.Windows.Forms.DataGridView dgvPrenda;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
     }
 }
