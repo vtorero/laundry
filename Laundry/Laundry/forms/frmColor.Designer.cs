10b03bbf5758670ba99c9612f2be949d7b3b6556
Victor Jimenez <vjimenez@americatv.com.pe>
2017-02-06 12:18:39 -0500

    laundry

---
+++ Laundry/Laundry/forms/frmColor.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace WindowsFormsApplication1.forms
+{
+    partial class frmColor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtNombreColor = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelValor = new System.Windows.Forms.Label();
+            this.txtValorColor = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // colorDialog1
+            // 
+            this.colorDialog1.SolidColorOnly = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(207, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(63, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Definir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtNombreColor
+            // 
+            this.txtNombreColor.Location = new System.Drawing.Point(95, 13);
+            this.txtNombreColor.Name = "txtNombreColor";
+            this.txtNombreColor.Size = new System.Drawing.Size(84, 20);
+            this.txtNombreColor.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nombre Color:";
+            // 
+            // labelValor
+            // 
+            this.labelValor.AutoSize = true;
+            this.labelValor.Location = new System.Drawing.Point(15, 44);
+            this.labelValor.Name = "labelValor";
+            this.labelValor.Size = new System.Drawing.Size(61, 13);
+            this.labelValor.TabIndex = 3;
+            this.labelValor.Text = "Valor Color:";
+            // 
+            // txtValorColor
+            // 
+            this.txtValorColor.Location = new System.Drawing.Point(95, 41);
+            this.txtValorColor.Name = "txtValorColor";
+            this.txtValorColor.Size = new System.Drawing.Size(85, 20);
+            this.txtValorColor.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(185, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(7, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "&";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtValorColor);
+            this.groupBox1.Controls.Add(this.labelValor);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtNombreColor);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(18, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(288, 130);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(175, 142);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(131, 56);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "&Guardar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // frmColor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 210);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmColor";
+            this.Text = "Mantenimiento de Colores";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtNombreColor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelValor;
+        private System.Windows.Forms.TextBox txtValorColor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ Laundry/Laundry/forms/frmColor.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApplication1.Persistencia;
+
+namespace WindowsFormsApplication1.forms
+{
+    public partial class frmColor : Form
+    {
+        public frmColor()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtValorColor.Text = colorDialog1.Color.Name;
+                label2.BackColor = colorDialog1.Color;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            WindowsFormsApplication1.Models.Color color = new WindowsFormsApplication1.Models.Color();
+            int resultado = 0;
+
+
+            if ((!string.IsNullOrWhiteSpace(txtNombreColor.Text)) && (!string.IsNullOrWhiteSpace(txtValorColor.Text)))
+            {
+                color.nombreColor = txtNombreColor.Text.Trim();
+                color.valorColor = txtValorColor.Text.Trim();
+                resultado = ColorDao.Agregar(color);
+            }
+            else
+            {
+                resultado = 0;
+                MessageBox.Show("Debe ingresar los valores");
+            }
+
+            if (resultado > 0)
+            {
+                
+                MessageBox.Show("Color Guardada Con Exito!!", "Guardado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("No se pudo guardar el Color", "Fallo!!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+    }
+}
